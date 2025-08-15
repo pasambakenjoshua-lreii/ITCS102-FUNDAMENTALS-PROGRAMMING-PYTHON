fe445aa702283ed7df184bcb5696c891b61bc3ac
Hanniel Maningas <pasambakenjoshua@gmail.com>
2025-08-15 22:36:35 +0800

    This is my file in Github

---
+++ activity6.py
@@ -0,0 +1,15 @@
+n1 = eval(input("Enter the first number : ")) 
+n2 = eval(input("Enter the second number : ")) 
+
+sum = n1 + n2
+dif = n1 - n2
+pro = n1 * n2
+quo = n1 / n2
+
+print("\nThe sum of", n1, "and", n2, "is", sum)
+print("The difference of", n1, "and", n2, "is", dif)
+print("The product of", n1, "and", n2, "is", pro)
+print("The quotient of", n1, "and", n2, "is", quo)
+print(n1, "exponent by", n2, "is", n1 ** n2)
+print("The remainder of", n1, "and", n2, "is", n1 % n2)
+print("The floor division of", n1, "and", n2, "is", n1 // n2) 	
