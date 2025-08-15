52bb1a3f40da9c9f4b4bf3dc64f87c4f883b9206
Hanniel Maningas <pasambakenjoshua@gmail.com>
2025-08-15 22:39:07 +0800

    This is my file in Github

---
+++ code_challenge1.py
@@ -0,0 +1,3 @@
+name = input("Type your name .? ")
+
+print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\t\tHi\t\t\t\t*\n\t\t*\t\t\t",name,"\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
