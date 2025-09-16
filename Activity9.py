ec283aa11ab15fbb6ce306e9e12f08fe9831cddc
Lreii <pasambakenjoshua@gmail.com>
2025-09-16 12:55:33 +0800

    Create Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,7 @@
+username = "kayle"
+pasword = "mississippi"
+
+#print (username == 'kayle')
+#print((username == 'kayle') and (password == 'mississippi'))
+#print((username == 'kayle') or (password == 'mississippi'))
+#print(not((username == 'kayle') or (password == 'mississippi')))
