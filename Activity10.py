64b01e8fa40cc55a5c2a7016a42b8bf2f9d10368
Lreii <pasambakenjoshua@gmail.com>
2025-09-16 12:56:17 +0800

    Create Activity10.py

---
+++ Activity10.py
@@ -0,0 +1,12 @@
+name = input("Input your name ---> ")
+isStudent = input("Are you a student (Yes / No) --> ")
+fare = eval(input("bayad --> "))
+
+if isStudent.lower() == "yes":
+	discount = fare * .2
+	new fare = fare discount
+	print("Hi", name, 11 student discount is", discount,
+" Discounted fare is ", new_fare)
+else:
+
+	print("Sorry", name," you are not eligible for student discount")
