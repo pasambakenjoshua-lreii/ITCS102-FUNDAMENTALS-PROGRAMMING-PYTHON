f04145d5ce45ae88ffcf9191de123f82264075a0
Lreii <pasambakenjoshua@gmail.com>
2025-10-13 10:43:38 +0800

    Create code_challenge10.py

---
+++ code_challenge10.py
@@ -0,0 +1,9 @@
+print("\t\t", end= "*")
+for x in range(1,12,1):
+    for y in range(12,x,-1):
+        print(" ", end=' ')
+    for z in range(1,x,1):
+        print("*", end=" ")
+    for t in range(1,x,1):
+        print("*", end=" ")
+    print()
