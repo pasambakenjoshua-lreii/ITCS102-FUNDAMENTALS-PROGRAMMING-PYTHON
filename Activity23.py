364e97a0d2cfa42c4c31f796dd1e32ab9763402a
Lreii <pasambakenjoshua@gmail.com>
2025-10-27 11:24:28 +0800

    Create Activity23.py

---
+++ Activity23.py
@@ -0,0 +1,20 @@
+brands = ['Shellby','Aston Martin', 'Maserati', 'Ford', 'Dodge']
+
+brands.sort()
+print(brands)
+brands.reverse()
+print(brands)
+brands.append('ferrari')
+print(brands)
+brands.pop()
+print(brands)
+brands.append('ferrarri')
+print(brands)
+for q in brands:
+    print(f"{q}, 2025")
+
+print("\n")
+print("+++++++++++++++++++++++++++++++++++++++++++")
+print("\n")
+
+full_name = 'Ken Joshua Pasamba'
